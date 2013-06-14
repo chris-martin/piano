4eb02947ada97cb7e5786387b17676bdb41bf27e
Christopher Martin <ch.martin@gmail.com>
2013-06-14 00:42:43 -0400

    typo

--- src/main/scala/play.scala
+++ src/main/scala/play.scala
@@ -4,7 +4,7 @@ object HelloWorld {
   def main(args: Array[String]) {
     ConfigFactory.load()
     val instrumentDir = "/usr/share/zynaddsubfx/banks/"
-    val instrument = instrumentDir + "SthPiano/0001-Soft Piano 1.xiz"
+    val instrument = instrumentDir + "SynthPiano/0001-Soft Piano 1.xiz"
     Seq("padsp", "zynaddsubfx", "-U", "-L", instrument)
     Seq("jackd", "-d", "alsa")
     Seq("aconnect", "-i")
