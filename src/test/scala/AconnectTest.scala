753de0d7dfd863d4635447c6f4cfd6d6832ec6e9
Christopher Martin <ch.martin@gmail.com>
2013-06-16 18:35:56 -0400

    add comment to aconnect test

--- src/test/scala/AconnectTest.scala
+++ src/test/scala/AconnectTest.scala
@@ -6,10 +6,26 @@ class AconnectTest extends FunSuite {
 
   test("parse inputs") {
     expect(Seq(
-      Client(0, "System", Seq(Port(0, "Timer"), Port(1, "Announce"))),
-      Client(14, "Midi Through", Seq(Port(0, "Midi Through Port-0"))),
-      Client(24, "KeyRig 49", Seq(Port(0, "KeyRig 49 MIDI 1"))),
-      Client(127, "crazy testin' thing", Seq(Port(24, "lolwut")))
+
+      Client(0, "System", Seq(
+        Port(0, "Timer"),
+        Port(1, "Announce")
+      )),
+
+      Client(14, "Midi Through", Seq(
+        Port(0, "Midi Through Port-0")
+      )),
+
+      Client(24, "KeyRig 49", Seq(
+        Port(0, "KeyRig 49 MIDI 1")
+      )),
+
+      // I don't know whether names actually can contain single quotes, but I added
+      // one with the assumption that single quotes are escaped by backslash.
+      Client(127, "crazy testin' thing", Seq(
+        Port(24, "lolwut")
+      ))
+
     )) {
       parseString(read("aconnect-inputs.txt"))
     }
@@ -18,8 +34,15 @@ class AconnectTest extends FunSuite {
 
   test("parse outputs") {
     expect(Seq(
-      Client(14, "Midi Through", Seq(Port(0, "Midi Through Port-0"))),
-      Client(128, "ZynAddSubFX", Seq(Port(0, "ZynAddSubFX")))
+
+      Client(14, "Midi Through", Seq(
+        Port(0, "Midi Through Port-0")
+      )),
+
+      Client(128, "ZynAddSubFX", Seq(
+        Port(0, "ZynAddSubFX")
+      ))
+
     )) {
       parseString(read("aconnect-outputs.txt"))
     }
